2417858c878fe60ca67e108cd3b006b53c22fe95
aniketkapdule <aniketkapdule12@gmail.com>
2022-05-07 00:31:49 +0530

    * Added the new logic for duplicates in DAG. * Added .gitignore file for cleaning

--- dags/emission-dag.py
+++ dags/emission-dag.py
@@ -1,6 +1,5 @@
 import os
 from functools import wraps
-from matplotlib.pyplot import table
 import pandas as pd
 from datetime import timedelta
 from airflow import DAG
@@ -57,9 +56,9 @@ def connect_db():
     cursor = conn.cursor()
     return engine, cursor
 
-def load(df, table_name): 
+def load(df, table_name, if_exists='append'): 
     db_engine, cursor = connect_db()
-    df.to_sql(table_name, db_engine, if_exists='append', index=False)
+    df.to_sql(table_name, db_engine, if_exists=if_exists, index=False)
     print(f'loaded {table_name}')
 
     #if table_name in ['drivers', 'cars']:
@@ -72,7 +71,9 @@ def load(df, table_name):
 def select_table_from_db(table):
     db_engine, cursor = connect_db()
     df = pd.read_sql_query(f"SELECT * from {table};", db_engine)
+    print(f"selected {table}")
     return df
+
 @logger
 def create_tables():
     db_engine, cursor = connect_db()
@@ -132,28 +133,34 @@ def extract(drivers, vehicle_fuel_consumptions, drivers_logbook):
     print("extracting the data")
     df_drivers_raw = pd.read_csv(drivers)
     df_veh_cons_raw = pd.read_csv(vehicle_fuel_consumptions)
-
+    df_drivers_logbook_raw = pd.read_csv(drivers_logbook)
     #it will check if the data is there or not, if the data is not there it will initiate an empty dataframe.
-    if os.path.isfile(drivers_logbook):
-        df_drivers_logbook_raw = pd.read_csv(drivers_logbook)
-        shutil.move("data/incoming_data/drivers_logbook.csv", "data/used_data/drivers_logbook.csv")
-    else:
-        print('No new data!')
-        #empty dataframe
-        df_drivers_logbook_raw = pd.DataFrame(columns=['brand', 'model', 'engine_size_l', 'cylinders', 'fuel_type',
-       'transmission', 'name', 'first_name', 'start_city', 'start_country',
-       'target_city', 'target_country', 'distance_km', 'date'])
+    #if os.path.isfile(drivers_logbook):
+    #    df_drivers_logbook_raw = pd.read_csv(drivers_logbook)
+    #    shutil.move("data/incoming_data/drivers_logbook.csv", "data/used_data/drivers_logbook.csv")
+    #else:
+    #    print('No new data!')
+    #    #empty dataframe
+    #    df_drivers_logbook_raw = pd.DataFrame(columns=['brand', 'model', 'engine_size_l', 'cylinders', 'fuel_type',
+    #   'transmission', 'name', 'first_name', 'start_city', 'start_country',
+    #   'target_city', 'target_country', 'distance_km', 'date'])
     return df_drivers_raw, df_veh_cons_raw, df_drivers_logbook_raw           
 
 @logger
 def transform_and_load():
+    db_engine, cursor = connect_db()
     df_drivers_raw, df_veh_cons_raw, df_drivers_logbook_raw = extract(drivers, vehicle_fuel_consumptions, drivers_logbook)
     print('Fetched the data')
     #driver table transformations
     df_drivers_clean = df_drivers_raw[['name', 'first_name']].sort_values('name').reset_index(drop=True)
     #deduping the data
     df_drivers_clean = df_drivers_clean.drop_duplicates(subset=['name', 'first_name']).reset_index(drop=True)
-    load(df_drivers_clean, table_name='drivers')
+    df_drivers_count = pd.read_sql_query("SELECT COUNT(driver_id) FROM drivers;", db_engine)
+    drivers_count = df_drivers_count.iloc[0].values[0]
+    if drivers_count == 0:
+        load(df_drivers_clean, table_name='drivers')
+    else:
+        pass
 
     #cars table
     df_veh_cons_raw.rename(columns={'BRAND':'brand', 'MODEL':'model', 'VEHICLE CLASS':'vehicle_class', 'ENGINE SIZE L':'engine_size_l', 'CYLINDERS':'cylinders',
@@ -164,38 +171,53 @@ def transform_and_load():
     df_veh_cons_raw = df_veh_cons_raw.reset_index(drop=True)
     #deduping
     df_cars_clean = df_veh_cons_raw.drop_duplicates(subset=['brand', 'model','vehicle_class', 'engine_size_l', 'cylinders', 'transmission', 'fuel_type']).reset_index(drop=True)
-    load(df_cars_clean, table_name='cars')
+    
+    # this logic should only be there if the car and drivers data is static.
+    df_cars_count = pd.read_sql_query("SELECT COUNT(car_id) FROM cars;", db_engine)
+    cars_count = df_cars_count.iloc[0].values[0]
+    if cars_count == 0:
+        load(df_cars_clean, table_name='cars')
+    else:
+        pass
+    
     load(df_drivers_logbook_raw, table_name='drivers_logbook_raw')
 
     df_cars_db = select_table_from_db(table="cars")
     df_drivers_db = select_table_from_db(table="drivers")
-    df_drivers_logbook_db = select_table_from_db(table="drivers_logbook_raw")
-
-    #transformation for drivers_logbook
-    df_drivers_logbook_deduped = df_drivers_logbook_db.drop_duplicates(subset=['brand', 'model', 'engine_size_l', 'cylinders', 'fuel_type',
-       'transmission', 'name', 'first_name', 'start_city', 'start_country',
-       'target_city', 'target_country', 'distance_km', 'date']).reset_index(drop=True)
     
+    # this if condition will check if we have received the new data or not.
     if len(df_drivers_logbook_raw) > 0:
-        df_car_log = df_cars_db.merge(df_drivers_logbook_deduped, how='right', on=['brand', 'model', 'engine_size_l', 'cylinders', 'fuel_type', 'transmission'])
-        #adding a total_emission column
+        df_car_log = df_cars_db.merge(df_drivers_logbook_raw, how='right', on=['brand', 'model', 'engine_size_l', 'cylinders', 'fuel_type', 'transmission'])
+        # adding a total_emission column
         values = df_car_log.distance_km * df_car_log.co2_emission_g_per_km
         df_car_log['total_emission'] = values
-        df_car_driver_log_clean = df_car_log.merge(df_drivers_db, how='left', on=['name', 'first_name'])[['car_id', 'driver_id', 'start_city', 'start_country', 'target_city',
-           'target_country', 'distance_km', 'date', 'total_emission']]
-        load(df_car_driver_log_clean, 'car_driver_log')
+        df_car_driver_log_raw = df_car_log.merge(df_drivers_db, how='left', on=['name', 'first_name'])[['car_id', 'driver_id', 'start_city', 'start_country', 'target_city',
+            'target_country', 'distance_km', 'date', 'total_emission']]
+        df_cdl_count = pd.read_sql_query("SELECT COUNT(car_id) FROM car_driver_log;", db_engine)
+        cdl_count = df_cdl_count.iloc[0].values[0]
+        if cdl_count == 0:
+            df_car_driver_log_clean = df_car_driver_log_raw.drop_duplicates(subset=['car_id', 'driver_id', 'start_city', 'start_country', 'target_country', 'target_city'])
+            load(df_car_driver_log_clean, table_name='car_driver_log')
+        else:
+            df_car_driver_log_db = select_table_from_db(table='car_driver_log')
+            df_car_driver_log_merge = df_car_driver_log_raw.merge(df_car_driver_log_db, on=['car_id', 'driver_id', \
+            'start_city', 'start_country', 'target_country', 'target_city'], how='left', indicator=True)
+            df_car_driver_log_clean = df_car_driver_log_merge[df_car_driver_log_merge['_merge'] == 'left_only'].drop(['distance_km_y', 'date_y', \
+            'total_emission_y', '_merge'], axis=1).rename(columns={'distance_km_x':'distance_km', 'date_x':'date', 'total_emission_x':'total_emission'})
+            df_car_driver_log_clean['date']= pd.to_datetime(df_car_driver_log_clean['date']) # making sure the datatype of date column
+            load(df_car_driver_log_clean, 'car_driver_log')
+            print(df_car_driver_log_clean)
     else:
         print('No new data!')
 
 with dag:
-    run_etl = PythonOperator(
-        task_id='run_etl',
-        python_callable=final
+    create_tables_if_not_exists = PythonOperator(
+        task_id='creating_tables_if_not_exists',
+        python_callable=create_tables
+    )
+    t_and_l = PythonOperator(
+        task_id='Transform_and_load',
+        python_callable=transform_and_load
     )
 
-run_etl
-
-
-
-
-
+create_tables_if_not_exists >> t_and_l
